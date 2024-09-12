e974be1c48b85f57a7557306cc535691407c0fd1
Goupil.François.emf <Francois.goupil@studentfr.ch>
2024-09-12 08:16:46 +0200

    groupe

--- exercices/exerciceDeGroupe/operateurLogiques.java
+++ exercices/exerciceDeGroupe/operateurLogiques.java
@@ -1,16 +1,20 @@
 
 public class operateurLogiques {
+
     public static void main(String[] args) {
         int opera = 0;
-        int opera2 = 2; 
-        System.out.println(!(opera == 1 )||(opera2 == 1 ));
-            
+        int opera2 = 2;
+        System.out.println((opera == 1) || (opera2 == 1));
+        // || (Ou) retourne « true » si au moins une des deux opérandes a et b est vraie.
+
+        System.out.println((opera == 1) && (opera2 == 1));
+        // L’opérateur Java && (Et) retourne « true » uniquement si chacune des opérandes a et b sont vraies.
+
+        System.out.println(!(opera == 1) || (opera2 == 1));
+        // !  La négation inverse la vérité d’un opérande de « true » à « false » ou vice versa.
+
         
 
-
-            
-        
     }
 
-    
 }
