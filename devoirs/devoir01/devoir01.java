b6f9b050580d177c29bfc54b4003bfed2ba7430a
Goupil.François.emf <Francois.goupil@studentfr.ch>
2024-09-12 09:22:13 +0200

    Devoir1 corrigé

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -1,15 +1,18 @@
 public class devoir01 {
     public static void main(String[] args) {
-        int monAge = 30;
+        int monAge = 12;
 
         //Relisez bien la donnée, int n'est pas tout à fait
         //le bon choix pour estMajeur. Il faut une variable 
         //qui permet de stocker si oui ou non la personne est majeure.
         //changez ceci et corrigez la suite de l'exercice
-        int estMajeur = 18;
-        System.out.println();
+        boolean estMajeur;
+        System.out.println("Je m'appel François Goupil");
+       
+        System.out.println("Mon age est de " + monAge + " ans");
 
-        if (monAge >= estMajeur){
+        if (monAge >= 18){
+            estMajeur = true;
             System.out.println("Je suis majeur");
         } else {System.out.println("Je ne suis pas encore majeur");
     }
