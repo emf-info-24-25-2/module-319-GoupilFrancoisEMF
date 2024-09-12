d21601d5cdb2d1ca24d2b1d0c679a9742bc08d9e
Goupil.François.emf <Francois.goupil@studentfr.ch>
2024-09-12 11:41:03 +0200

    exercice08

--- exercices/exercice08/ExercieBoucles2/src/App.java
+++ exercices/exercice08/ExercieBoucles2/src/App.java
@@ -1,32 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int i = 5;
-        System.out.println("Boucle for:");
-        for (i = 5; i > 0;){
-            System.out.println(i);
-            i--;
-            if (i == 0){
-                System.out.println("Décollage !!!");
-            }
-        }
-        System.out.println("Boucle While:");
-        i = 5;
-        while(i > 0){
-            System.out.println(i);
-            i--;
-            if (i == 0){
-                System.out.println("Décollage !!");
-            }
-        }
-        i = 5;
-        System.out.println("Boucle do-while:");
-        do { 
-            System.out.println(i);
-            i--;
-            if (i == 0){
-                System.out.println("Décollage !!");
-            }
-        } while (i > 0);
-
+        
     }
 }
