6daee7c2a9fed5c8d0c35c9e8256cc9f660dd84c
Fanny Riedo <141828170+riedo-emf@users.noreply.github.com>
2024-11-13 21:44:51 +0100

    Bien!

--- devoirs/devoir05/devoir05.java
+++ devoirs/devoir05/devoir05.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class devoir05 {
     public static void main(String[] args) {
+        //RIF: ici vous faites un nombre entre 1 et 39
         int nombreATrouver = (int) (Math.random() * (39 - 1 + 1)) + 1;
         int tentative = 0;
         int nmbrEssai = 0;
@@ -13,6 +14,8 @@ public class devoir05 {
         while (!trouve) {
             System.out.print("Entrez votre nombre : ");
             tentative = scanner.nextInt();
+
+            //RIF: la ligne suivante n'est pas nécessaire
             scanner.nextLine();
 
             if (tentative < nombreATrouver) {
